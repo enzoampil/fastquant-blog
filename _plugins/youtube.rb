009802f7e600f59d1793c3f74a23b33f876a9bc9
Lorenzo Ampil <lorenzojulioampil@gmail.com>
2020-07-26 01:37:16 +0800

    Add youtuve plugin

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -1,5 +1,5 @@
 class YouTube < Liquid::Tag
-  Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
+  Syntax = /^\s*([^\s]+)(?:\s+(\d+)\s+(\d+)\s*)?/
 
   def initialize(tagName, markup, tokens)
     super
@@ -21,7 +21,7 @@ class YouTube < Liquid::Tag
 
   def render(context)
     # "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\"allowfullscreen></iframe>"
-    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}?color=white&theme=light\"></iframe>"
+    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}?color=white&amp;theme=light\">&nbsp;</iframe>"
   end
 
   Liquid::Template.register_tag "youtube", self
